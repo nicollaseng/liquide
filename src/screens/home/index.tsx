48cc20fc6d9930fcb77989b4bcfb9b7ec62518e4
Nicollas Matheus <nicollas@nicollasmatheus.com>
2020-10-20 02:44:17 -0300

    feat: equipments list scroll view

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { requestEquipment } from '../../store/actions/equipments.action';
+import { RootState } from '../../store/reducer';
+
+import * as home from './styles';
+
+interface IEquipments {
+	name: string
+};
 
 const Home = () => {
-    return <></>
-}
+	const epis = useSelector<RootState, Array<IEquipments>>(state => state.equipments.epi)
+  const dispatch = useDispatch()
+  
+  useEffect(() => {
+    initializeComponent();
+  }, []);
+
+	const initializeComponent = () => dispatch(requestEquipment());
+	
+  return (
+    <home.WrapperContainer>
+    	<home.Scroll>
+			<home.Text mb={20} bold size={3}>Equipamentos de Segurança</home.Text>
+				{epis.length > 0 && epis.map((epi: IEquipments) => (
+					<home.Text>{epi.name}</home.Text>
+				))}
+			</home.Scroll>
+    </home.WrapperContainer>
+  )
+};
 
 export default Home;
---
+++ src/screens/home/styles.ts
@@ -0,0 +1,31 @@
+import styled from 'styled-components/native'
+
+const normalizerFontSize = (size: number | undefined) => {
+	const sizes = [12, 15, 18];
+	const normalizedSize = sizes[size ?? 0] ?? 12;
+
+	return normalizedSize
+}
+
+export const WrapperContainer = styled.View`
+	flex: 1;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+`;
+
+export const Scroll = styled.ScrollView.attrs({
+	flex: 1,
+	alignItems: 'center',
+	justifyContent: 'center'
+})``;
+
+export const Text = styled.Text<{
+	bold?: boolean;
+	size?: number;
+	mb?: number;
+}>`
+	margin-bottom: ${props => props.mb ?? 0};
+	font-weight: ${props => props.bold ? 'bold' : 300};
+	font-size: ${props => normalizerFontSize(props.size)};
+`;
